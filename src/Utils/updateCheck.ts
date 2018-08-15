591479abbce47a61421c63859063e252dd09ef4f
Steve Axtmann <steve@axtmann.me>
2018-08-15 21:42:35 +0200

    check for updates more frequently

--- src/Utils/updateCheck.ts
+++ src/Utils/updateCheck.ts
@@ -2,9 +2,10 @@ import updateNotifier from 'update-notifier';
 
 export default function () {
     const pkg = require('../../package.json');
+    const updateCheckInterval = 1000 * 60 * 60 * 12;
 
     // Checks for available update and returns an instance
-    const notifier = updateNotifier({pkg});
+    const notifier = updateNotifier({pkg, updateCheckInterval});
 
     // Notify using the built-in convenience method
     notifier.notify({isGlobal: true});

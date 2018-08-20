c5e7802b2b9a554f66c0e3c24531d0188afccd26
Steve Axtmann <steve@axtmann.me>
2018-08-20 21:06:27 +0200

    attempt #1 to separate the table

--- src/Renderers/TableRenderer.ts
+++ src/Renderers/TableRenderer.ts
@@ -33,15 +33,17 @@ export default class TableRenderer implements Renderer {
         const timeLeft: any = (under ? '' : '+') + TableRenderer.convertTime(remaining);
 
 
-        const tableToDisplay = [
+        const weekTable = [
             ['', 'Day', 'Week'],
             ['Break' + (running === 'break' ? ' *' : ''), TableRenderer.convertTime(this._lastDay.break), TableRenderer.convertTime(this._times.break)],
             ['Work' + (running === 'work' ? ' *' : ''), TableRenderer.convertTime(this._lastDay.work), TableRenderer.convertTime(this._times.work)],
+        ];
+        const todayTable = [
             [under ? 'Left' : 'Over', under ? timeLeft : timeLeft.green, exact]
         ];
         const options = {
             drawHorizontalLine: (index, size) => {
-                return index < 2 || index > size - 2;
+                return index < 2 || index === size;
             },
             columns: {
                 1: {
@@ -53,6 +55,6 @@ export default class TableRenderer implements Renderer {
             }
         };
 
-        logUpdate(table.table(tableToDisplay, options));
+        logUpdate(table.table(weekTable, options) + table.table(todayTable));
     }
 }
